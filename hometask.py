278191b588a9b95baaddb289991a705c8459cfab
irvus <iatarasenko@edu.hse.ru>
2021-06-04 02:27:21 +0300

    Корректно предварительно обрабатывается корректная цсвшка

---
+++ hometask.py
@@ -0,0 +1,44 @@
+"""
+Created on Fri Jun 4 02:25:13 2021
+
+@author: ilia
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+space_missions = pd.read_csv("Space_Corrected.csv")
+
+space_missions['Year'] = space_missions['Datum'].apply(lambda x: int(str(x).split()[3]))  # выцепляю интовый год
+space_missions['Month'] = space_missions['Datum'].apply(lambda x: str(x).split()[1])  # выцепляю месяц
+space_missions['Country'] = space_missions['Location'].apply(lambda x: str(x).split()[-1])
+
+for i in range(space_missions.shape[0]):  # Make USSR Great Again!
+    if space_missions.at[i, 'Year'] <= 1991 and (space_missions.at[i, 'Country'] == 'Russia' or
+                                                 space_missions.at[i, 'Country'] == 'Kazakhstan'):
+        space_missions.at[i, 'Country'] = 'USSR'
+
+
+
+plt.figure(figsize=(20, 6))
+space_missions['Launch Date_year'].value_counts().plot(kind='bar')
+plt.xticks(rotation=90)
+plt.title('Number of launches per year')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+space_missions['Landing Outcome'].value_counts().plot(kind='bar')
+plt.title('Number of successful/failed landings')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+space_missions['Customer Country'].value_counts().plot(kind='bar')
+plt.title('Distribution by customer countries')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+space_missions['Launch Site'].value_counts().plot(kind='bar')
+plt.title('Distribution by launch sites')
+plt.show()
