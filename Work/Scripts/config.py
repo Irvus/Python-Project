6998f89267deac9c91a9cbddb4c84bb70431f986
Эвелина Маркова <esmarkova_1@miem.hse.ru>
2021-06-14 11:52:23 +0300

    14.06.21 11:52

--- Work/Scripts/config.py
+++ Work/Scripts/config.py
@@ -1,5 +1,8 @@
 import os
 PWD = "Data/Space_Corrected.csv"
 
-my_path = os.path.abspath(__file__)
-
+import os
+path = os.path.abspath(os.path.join(os.getcwd(), '..', 'Data'))
+path_to_csv = path + '/Space_Corrected.csv'
+path_to_new_csv = path + '/space_missions.csv'
+path_graph = os.path.abspath(os.path.join(os.getcwd(), '..', 'Graphics'))
